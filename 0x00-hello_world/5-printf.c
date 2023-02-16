c831aee660030cf30f47c57988dd2ca27f15a13e
Cpotate <Mfon.11@aol.com>
2023-02-16 10:25:28 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * main - A program that print a line using the printf function
- * Return: 0 (Success)
+ * *Description: main - prints out a string to stdout
+ * *Return: 0 if success
  */
-int main (void)
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-	return (0);
-}
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);

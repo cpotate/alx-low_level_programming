faa428c1861a1629ba64c69337ad4649ed61c645
Cpotate <Mfon.11@aol.com>
2023-03-13 08:23:01 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
 /**
- * _strdup - duplicate to new memory space location
- * @str: char
- * Return: 0
+ *  _strdup - duplicate to new memory space location
+ *  @str: char
+ *  Return: 0
  */
 char *_strdup(char *str)
 {
@@ -21,10 +20,11 @@ char *_strdup(char *str)
 	aaa = malloc(sizeof(char) * (i + 1));
 
 	if (aaa == NULL)
-		if (aaa == NULL)
+		return (NULL);
 
 	for (r = 0; str[r]; r++)
 		aaa[r] = str[r];
 
 	return (aaa);
+
 }
